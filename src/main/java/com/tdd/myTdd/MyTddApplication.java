7b265e942cea704fe37d86ed7a12b4b1957f4ffc
nashs789 <bbock1214@gmail.com>
2024-10-19 18:18:41 +0900

    Initial commit

---
+++ src/main/java/com/tdd/myTdd/MyTddApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.myTdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/myTdd/MyTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.myTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
